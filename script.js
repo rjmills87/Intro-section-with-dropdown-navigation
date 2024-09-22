0921a77e2dafd4648e41cfea04729dc02715a9d4
Rhys Miller <rjmills87@aol.com>
2024-09-22 21:45:23 +0100

    Replaced removed JS code for replacing hero image and implemented use of <picture> tag instead for better responsive performance.

--- script.js
+++
@@ -1,8 +0,0 @@
-const heroImage = document.getElementById("hero-img");
-
-// Change Hero image dependent on viewport size
-if (window.matchMedia("(max-width: 480px)").matches) {
-  heroImage.src = "images/image-hero-mobile.png";
-} else {
-  heroImage.src = "images/image-hero-desktop.png";
-}
